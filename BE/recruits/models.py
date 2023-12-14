e1c9e4717946031527bfbc7d52778b679ae2980e
jaemin.kim <jmkim648@gmail.com>
2023-12-15 01:00:10 +0900

    :sparkles: Feat: 모집/커뮤 게시글 모델 작성 ( #13 )
    ## 생성한 파일
    
    posts > models.py
    recruits > models.py

--- BE/recruits/models.py
+++ BE/recruits/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from teams.models import Team
 
-# Create your models here.
+class RecruitmentPost(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    view_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
