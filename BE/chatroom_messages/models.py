95a9d4d4aecb32c1fe5d40254c43a8bb0800368a
nyloncode <dybrkr@naver.com>
2023-12-15 13:08:34 +0900

    :fire: Remove: ChatRoomMessage id 필드 제거
    django에서 id가 기본적으로 설정되기에 제거하기로 결정

--- BE/chatroom_messages/models.py
+++ BE/chatroom_messages/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 from chatrooms.models import ChatRoom
 
 class ChatRoomMessage(models.Model):
-    id = models.BigAutoField(primary_key=True)
     content = models.TextField()
     chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
