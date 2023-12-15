3cf37b4ee787a210c7272e8c31b95b148da37c31
nyloncode <dybrkr@naver.com>
2023-12-15 11:20:47 +0900

    :sparkles: Feat: ChatRoom 모델 추가

--- BE/chatrooms/models.py
+++ BE/chatrooms/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from teams.models import Team
 
-# Create your models here.
+class ChatRoom(models.Model):
+    """
+    실시간 채팅방 모델
+
+    Attributes:
+        team (ForeignKey): Team 모델과의 1:M 관계
+    """
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}-{self.team}"
