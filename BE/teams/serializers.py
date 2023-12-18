4f1fe8c1f58cfdd7b3d2cc94811404e856d5438e
jaemin.kim <jmkim648@gmail.com>
2023-12-19 00:28:42 +0900

    :sparkles: Feat: 모임 앱 API구현
    ## 생성 및 수정한 파일
    teams 앱 일체
    schedules > model 수정
    
    ## Detail
    Team(모임) 모델의 URL 설계에 맞춰 API를 구현하였습니다.
    일정 모델 중 Date(특정 날짜 지정) 기능을 제거하기로 해 수정하였습니다.
    이로인해 기존 1:N관계였던 모임:일정 관계가 1:1관계가 되었습니다.
    일정 앱에서 할 예정이었던 '일정 수정' 기능을 모임의 Detail 수정 때 같이 하도록 통합하였습니다.
    
    ## 비고
    모임 정보 조회 시 일정 모델이 Join 되는 형태가 되었기 때문에 FE 작업시 응답값을 미리 확인해 둘 필요가 있을 것 같습니다.
    
    Thunder Client와 직접 관리자 페이지에서 생성한 DB로 테스트 완료하였습니다.
    다만 로그인이 구현된 페이지에서 요청을 전송해보지는 않아 아직 권한 부분이 미확인되었습니다.
    내일 중 FE구현된 환경에서 로그인 후 테스트 마친 후 문제 있을 시 수정하겠습니다.

---
+++ BE/teams/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from schedules.models import Schedule
+from django.contrib.auth import get_user_model
+from .models import Team, TeamMember
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['name', 'category', 'is_closed', 'location', 'max_attendance', 'current_attendance', 'introduce', 'image', 'member']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'email', 'nickname', 'profile_image']
+
+
+class TeamMemberSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = TeamMember
+        fields = ['user', 'is_leader', 'is_approved']
+
+
+class TeamMemberChangeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TeamMember
+        fields = ['team', 'user', 'is_leader']
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    day = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+    week = serializers.ListField(child=serializers.CharField(), allow_empty=True)
+
+    class Meta:
+        model = Schedule
+        fields = ['frequency', 'day', 'week', 'start_time', 'end_time']
+
+class TeamDetailSerializer(serializers.ModelSerializer):
+    schedule = ScheduleSerializer()
+
+    class Meta:
+        model = Team
+        fields = ['name', 'category', 'is_closed', 'location', 'max_attendance', 'current_attendance', 'introduce', 'image', 'schedule']
+    
+    def update(self, instance, validated_data):
+        schedule_data = validated_data.pop('schedule', {})
+        schedule_instance = instance.schedule
+
+        # schedule 데이터 업데이트
+        for key, value in schedule_data.items():
+            setattr(schedule_instance, key, value)
+
+        # 유효성 검사를 통한 데이터 저장
+        schedule_serializer = ScheduleSerializer(instance=schedule_instance, data=schedule_data)
+        schedule_serializer.is_valid(raise_exception=True)
+        schedule_serializer.save()
+
+        # Team 모델의 나머지 필드 업데이트
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+            
+        instance.save()
+
+        return instance
