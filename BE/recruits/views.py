9488afafb1e6e13cf389395a76ff6b97e1162647
jaemin.kim <jmkim648@gmail.com>
2023-12-20 05:08:57 +0900

    :sparkles: Feat: 모집 게시글 생성 기능 구현 및 모임 인원 수 관련 기능 구현 ( #54, #57)
    # 수정한 파일
    teams > serializers.py
    teams > views.py
    recruits > models.py
    recruits > serializers.py
    recruits > views.py
    
    ## 수정한 내용
    teams 앱의 구성원 승인, 추방, 탈퇴 요청 시 current_attendance 수정 기능 적용
    recruits 앱의 모집 게시글 생성 기능 -> 모임, 일정 생성 연동
    recruits model에 'region(지역)' 필드가 빠져있어 추가
    
    ## 비고
    테스트를 아직 진행하지 못했습니다.
    오전 중 테스트 후 문제가 생기면 바로 수정하겠습니다.

--- BE/recruits/views.py
+++ BE/recruits/views.py
@@ -3,9 +3,9 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import RecruitmentPost
-from .serializers import RecruitmentPostSerializer, TeamMemberSerializer
+from .serializers import RecruitmentPostSerializer, RecruitmentPostCreateSerializer
 from teams.models import Team, TeamMember
-from teams.serializers import TeamSerializer, ScheduleSerializer
+from teams.serializers import TeamSerializer, ScheduleSerializer, TeamMemberSerializer
 from schedules.models import Schedule
 
 class RecruitmentPostViewSet(viewsets.ModelViewSet):
@@ -31,7 +31,50 @@ class RecruitmentPostViewSet(viewsets.ModelViewSet):
         '''
         게시글 생성 시 Team과 Schedule 생성
         '''
-        pass
+        serializer = RecruitmentPostCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        # 모임 정보가 입력되어 있지 않을 경우 모임 생성
+        if not serializer.validated_data['team']:
+            user = serializer.validated_data['user']
+            team_data = {
+                'name': f"{user.nickname}의 모임",
+                'category': serializer.validated_data['category'],
+                'max_attendance': serializer.validated_data['max_attendance'],
+                'current_attendance': 1,
+            }
+            team_serializer = TeamSerializer(data=team_data)
+            team_serializer.is_valid(raise_exception=True)
+            team_serializer.save()
+
+            # 작성자 모임 리더 등록
+            team_member_data = {
+                'user': user.id,
+                'team': team_serializer.data['id'],
+                'is_leader': True,
+                'is_approved': True,
+            }
+            team_member_serializer = TeamMemberSerializer(data=team_member_data)
+            team_member_serializer.is_valid(raise_exception=True)
+            team_member_serializer.save()
+            serializer.validated_data['team'] = team_serializer.data['id']
+
+        # 모집 게시물 생성
+        self.perform_create(serializer)
+
+        # 일정 생성
+        schedule_data = {
+            'team': serializer.validated_data['team'],
+            'frequency': serializer.validated_data['frequency'],
+            'week': serializer.validated_data['week'],
+            'day': serializer.validated_data['day']
+        }
+        schedule_serializer = ScheduleSerializer(data=schedule_data)
+        schedule_serializer.is_valid(raise_exception=True)
+        schedule_serializer.save()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
     @action(detail=False, methods=['get'], url_path='hot', url_name='hot')
     def hot_list(self, request):
@@ -39,6 +82,8 @@ class RecruitmentPostViewSet(viewsets.ModelViewSet):
         인기 모집목록 조회 기능
         정렬 기준 : view_count
         모집 개수 : count 변수
+
+        페이지네이션 적용 필요
         '''
         count = 5
         queryset = RecruitmentPost.objects.order_by('-view_count')[:count]
