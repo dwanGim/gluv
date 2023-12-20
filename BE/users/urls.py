17f866350e3e3176da79f32dbfacdc3007259e49
jaemin.kim <jmkim648@gmail.com>
2023-12-20 17:15:17 +0900

    :bug: Fix: users 앱 테스트 및 버그 수정
    ## 수정한 파일
    users app
    
    ## 수정한 내용
    view에서 UserPassesTestMixin를 사용할경우 특정 url로 redirect시킵니다. 권한을 따로 설정했습니다.
    회원정보 수정의 경우 url이 명세와 어긋나 있어 수정했습니다.
    회원정보 수정 시 Request Data와 Response Data의 수정이 필요해 작업했습니다.

--- BE/users/urls.py
+++ BE/users/urls.py
@@ -14,10 +14,10 @@ from .views import (
 )
 
 urlpatterns = [
-    path('signup/', UserCreateView.as_view(), name='signup'), # 회원가입
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # 로그인
-    path('logout/', UserLogoutView.as_view(), name='logout'), # 로그아웃
-    path('<int:pk>/profile/', UserDetailView.as_view(), name='user_detail'), # 프로필
-    path('<int:pk>/profile/edit/', UserProfileEditView.as_view(), name='user_profile_edit'), # 프로필 수정
-    path('deactivate/', UserDeactivateView.as_view(), name='user_deactivate'), # 회원탈퇴
+    path('signup/', UserCreateView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('<int:pk>/profile/', UserDetailView.as_view(), name='user_detail'),
+    path('profile/', UserProfileEditView.as_view(), name='user_profile_edit'),
+    path('deactivate/', UserDeactivateView.as_view(), name='user_deactivate'),
 ]
