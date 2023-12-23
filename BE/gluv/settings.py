1a5da830596f1eeb98cd2772b53afc5ed8f2c4f1
dwanGim <nugoory20@gmail.com>
2023-12-23 17:47:29 +0900

    :spakles: Fix : 토큰갱신 기능 추가 중

--- BE/gluv/settings.py
+++ BE/gluv/settings.py
@@ -152,6 +152,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
 }
 
 AUTH_USER_MODEL = 'users.User'
